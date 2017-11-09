ca71bbde41152d7a06322b0985d24345adde9d1c
Thomas Senez <thomas.senez@airdates.co>
2017-11-09 11:50:49 +0100

    start activity

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -1,5 +1,6 @@
 package fr.android.androidexercises;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -23,8 +24,8 @@ public class LibraryActivity extends AppCompatActivity {
         openButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Replace with startActivity() to start BookActivity
-                Toast.makeText(LibraryActivity.this, R.string.toast_todo, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LibraryActivity.this, BookActivity.class);
+                startActivityForResult(intent, 42);
             }
         });
     }
