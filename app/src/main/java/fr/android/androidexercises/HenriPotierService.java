b2771a5f84cc9659be5de80d16d2d12d14957ffd
Thomas Senez <thomas.senez@airdates.co>
2017-11-10 14:04:29 +0100

    API

--- app/src/main/java/fr/android/androidexercises/HenriPotierService.java
+++ app/src/main/java/fr/android/androidexercises/HenriPotierService.java
@@ -1,7 +1,13 @@
 package fr.android.androidexercises;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface HenriPotierService {
 
-    // TODO Method GET books which return a List<Book>
+    @GET("books")
+    Call<List<Book>> listBooks();
 
 }
--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -3,6 +3,13 @@ package fr.android.androidexercises;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import timber.log.Timber;
 
 public class LibraryActivity extends AppCompatActivity {
@@ -15,13 +22,28 @@ public class LibraryActivity extends AppCompatActivity {
         // Plant logger cf. Android Timber
         Timber.plant(new Timber.DebugTree());
 
-        // TODO build Retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://henri-potier.xebia.fr")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // TODO create a service
+        HenriPotierService service = retrofit.create(HenriPotierService.class);
 
-        // TODO listBooks()
+        service.listBooks().enqueue(new Callback<List<Book>>() {
+            @Override
+            public void onResponse(Call<List<Book>> call, Response<List<Book>> response) {
+                for (Book book : response.body()){
+                    Timber.d(book.getTitle());
+                }
+            }
 
-        // TODO enqueue call and display book title
+            @Override
+            public void onFailure(Call<List<Book>> call, Throwable t) {
+                Timber.d(t);
+            }
+        });
+
+
 
         // TODO log books
 
