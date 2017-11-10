075b56f254c27f827050153fa964e4a888005944
Thomas Senez <thomas.senez@airdates.co>
2017-11-10 10:56:53 +0100

    fragments

--- app/src/main/java/fr/android/androidexercises/LibraryActivity.java
+++ app/src/main/java/fr/android/androidexercises/LibraryActivity.java
@@ -9,12 +9,17 @@ public class LibraryActivity extends AppCompatActivity implements Step0Fragment.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
-        // TODO replace Step0Fragment in containerFrameLayout
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerFrameLayout, new Step0Fragment(), Step0Fragment.class.getSimpleName())
+                .commit();
     }
 
     @Override
     public void onNext() {
-        // TODO replace Step1Fragment in containerFrameLayout
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.containerFrameLayout, new Step1Fragment(), Step1Fragment.class.getSimpleName())
+                .commit();
     }
 
 }
